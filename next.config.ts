31dda4317d408085b471a5d4c320c5bf9eefa0e3
Krishan . <krishan@fnz.com>
2026-04-27 18:26:56 +0100

    added docker configs

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   reactCompiler: true,
   serverExternalPackages: [
     "@chroma-core/google-gemini",
     "chromadb",
     "pdf-parse",
   ],
+  turbopack: {
+    ignoreIssue: [
+      {
+        path: /node_modules[\/\\]@chroma-core[\/\\]google-gemini/,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
